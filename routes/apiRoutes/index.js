b91ce9e0a09a20d27d9f164083f24a1b54c4d87f
James Geib <jpgeib2@gmail.com>
2020-05-13 16:21:46 -0700

    created api routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,53 @@
 const router = require("express").Router();
 
+const workout = require("./../../models/Workout");
 
+router.get("/api/workouts", (req, res) => {
+    workout.find().then(
+        dbWorkout => {
+            res.json(dbWorkout);
+        }
+    ).catch(
+        err => {
+            res.json(err);
+        }
+    )
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    workout.find({}).limit(7).then(
+        dbWorkout => {
+            res.json(dbWorkout);
+        }
+    ).catch(
+        err => {
+            res.json(err);
+        }
+    )
+});
+
+router.post("/api/workouts", (req, res) => {
+    workout.create(req.body).then(
+        dbWorkout => {
+            res.json(dbWorkout);
+        }
+    ).catch(
+        err => {
+            res.json(err);
+        }
+    )
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }).then(
+        dbWorkout => {
+            res.json(dbWorkout);
+        }
+    ).catch(
+        err => {
+            res.json(err);
+        }
+    )
+});
 
 module.exports = router;
