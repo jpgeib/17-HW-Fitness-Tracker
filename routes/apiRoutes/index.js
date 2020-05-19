2eb33a83188cab018076ffcec7dc99cb21ace211
James Geib <jpgeib2@gmail.com>
2020-05-18 17:59:01 -0700

    changes to the apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const workout = require("./../../models/Workout");
 
-router.get("/api/workouts", (req, res) => {
+router.get("/workouts", (req, res) => {
     workout.find().then(
         dbWorkout => {
             res.json(dbWorkout);
@@ -14,7 +14,7 @@ router.get("/api/workouts", (req, res) => {
     )
 });
 
-router.get("/api/workouts/range", (req, res) => {
+router.get("/workouts/range", (req, res) => {
     workout.find({}).limit(7).then(
         dbWorkout => {
             res.json(dbWorkout);
@@ -26,7 +26,7 @@ router.get("/api/workouts/range", (req, res) => {
     )
 });
 
-router.post("/api/workouts", (req, res) => {
+router.post("/workouts", (req, res) => {
     console.log(req.body, "Inside API Routes");
     workout.create(req.body).then(
         dbWorkout => {
@@ -39,7 +39,7 @@ router.post("/api/workouts", (req, res) => {
     )
 });
 
-router.put("/api/workouts/:id", (req, res) => {
+router.put("/workouts/:id", (req, res) => {
     workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }).then(
         dbWorkout => {
             res.json(dbWorkout);
